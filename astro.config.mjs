855727c5f321829532500287e96389c865191641
cwang13 <cwang13@paypal.com>
2025-01-15 11:32:46 +0800

    test

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://eureka00zero.github.io/BBlog",
+  integrations: [mdx(), sitemap()],
+});
