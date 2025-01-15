a7f7556c5eea7aa02c85be0320c6ba7a82f2a904
cwang13 <cwang13@paypal.com>
2025-01-15 11:39:57 +0800

    test build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://eureka00zero.github.io/BBlog",
+  site: "https://eureka00zero.github.io",
   base: "BBlog",
   integrations: [mdx(), sitemap()],
 });
